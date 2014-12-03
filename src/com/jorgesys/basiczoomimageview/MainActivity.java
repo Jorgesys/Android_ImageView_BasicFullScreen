6197cfc779f59387698291b35caf3cbb7db09155
Jorgesys <tuna.puisor@gmail.com>
2014-12-03 12:15:22 -0600

    no message

---
+++ src/com/jorgesys/basiczoomimageview/MainActivity.java
@@ -0,0 +1,62 @@
+package com.jorgesys.basiczoomimageview;
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	private boolean isImageFitToScreen =  false;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		
+		final ImageView imageView  = (ImageView)findViewById(R.id.imageView1);
+		imageView .setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {				
+			      if(isImageFitToScreen) {
+						Toast.makeText(getApplicationContext(), "NORMAL SIZE!", Toast.LENGTH_SHORT).show();
+	                    imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+	                    imageView.setAdjustViewBounds(true);
+	                    isImageFitToScreen = false;
+	                }else{
+	    				Toast.makeText(getApplicationContext(), "FULLSCREEN!", Toast.LENGTH_SHORT).show();	                    
+	                    imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+	                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+	                    isImageFitToScreen = true;
+	                }
+	            }         			
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
